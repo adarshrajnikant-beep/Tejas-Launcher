ac9346d14aa62ba82ebec0054c07793eee21f932
adarshrajnikant-beep <your-adarshrajnikant@gmail.com>
2026-09-12 01:02:49 +0530

    Fix: Add missing TejasKesari, TejasAshoka and theme utils

---
+++ app/src/main/java/org/tejasos/launcher/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package org.tejasos.launcher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TejasKesari = Color(0xFFFF6F00)
+val tejasKesari = Color(0xFFFF6F00)
+
+val TejasAshoka = Color(0xFF1E88E5)
+val tejasAshoka = Color(0xFF1E88E5)
+
+val TejasHarita = Color(0xFF2E7D32)
+val tejasHarita = Color(0xFF2E7D32)
+
+val TejasShwet = Color(0xFFFFFFFF)
+val tejasShwet = Color(0xFFFFFFFF)
+
+val TejasDark = Color(0xFF0A0E17)
+val TejasCard = Color(0xFF161B22)
